d54f063af57e5b6d25b004e6e4799a1f1c433169
jhemin0415 <37057873+jhemin0415@users.noreply.github.com>
2024-04-03 09:38:55 +0900

    프론트엔드 진입점 라우터 흐름 설계
    랜딩 페이지 -> 로그인 -> 파일 업로드 -> 이미지 확인 -> 어노테이션 작성
    -> 저장

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Button from "@mui/material/Button";
+import NavBar from "./components/nav_bar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />}></Route>
+                    <Route path="signin" element={<Signin />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
