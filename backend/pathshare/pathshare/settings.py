8f45c136d2164d222240b13f7112a200dbfb8d94
jhemin0415 <37057873+jhemin0415@users.noreply.github.com>
2024-04-09 20:27:43 +0900

    앱 배포를 위한 django-daphne, react-nginx 로 배치 변경, 실행 확인

--- backend/pathshare/pathshare/settings.py
+++ backend/pathshare/pathshare/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'account',
-    'webpack_loader'
+    
 ]
 
 WEBPACK_LOADER = {
--- backend/pathshare/start.sh
+++ backend/pathshare/start.sh
@@ -5,6 +5,6 @@ echo start server
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 
 daphne -b 0.0.0.0 -p 8000 pathshare.asgi:application
